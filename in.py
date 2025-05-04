eb1bfddc277ef812998974d8e90fe0ac9e7010d8
Harsh Jyoriya <harshjyoriyagdg24@gmail.com>
2025-05-04 22:22:11 +0530

    Pandas

--- in.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df= pd.read_json("sample_data.json")
-
-print("Displaying the info of Data set:")
-print(df.info())
--- row.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df= pd.read_json("sample_data.json")
-
-print("Display 10 rows of first")
-print(df.head())
-
-print("Display 10 rows of last")
-print(df.tail())
